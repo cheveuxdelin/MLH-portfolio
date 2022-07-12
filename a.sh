71207e1fd8adc001b187f52337ae34cc9f3d7ac7
cheveuxdelin <victorba2120@gmail.com>
2022-07-12 14:56:24 -0700

    removed files

--- a.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /root/MLH-portfolio
-git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip3 install -r requirements.txt
-systemctl daemon-reload
-systemctl restart myportfolio
--- curl-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-curl --location --request POST 'http://127.0.0.1:5000/api/timeline_post?name=mari&email=a@a.com&content=esto es un post' \
---header 'Authorization: Bearer asdfasdsdfa' \
---form 'name="hola"' \
---form 'email="hola"' \
---form 'content="hola"'
-
-curl http://127.0.0.1:5000/api/timeline_post
